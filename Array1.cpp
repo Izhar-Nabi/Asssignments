60b47f3f7fb7a4b88b551d74ebc5a1f77717fc35
Izhar-Zour <90437474+Izhar-Nabi@users.noreply.github.com>
2021-09-19 12:44:32 +0500

    Array Program

---
+++ Array1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string array[4]= {"izhar", "Haseeb", "Israr", "Hanan"};
+	for(int i=0; i<=4; i++ ){
+	cout<<array[i]<<"\n";
+	}
+//	for(int j=1; j<2; j++)
+//	cout<<"\n These Are All Called as  hawali Sagnt"<<array[j];
+	return 0;
+}
